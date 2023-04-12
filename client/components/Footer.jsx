0000748b67ab32f01b6a9dbda1446261d002b250
surojeet ghosh <surojeetghosh89@gmail.com>
2023-04-12 22:06:05 +0530

    Added Footer

---
+++ client/components/Footer.jsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+    return(
+        <div className="footer bg-dark text-light p-2 text-center shadow-lg">
+            <p className="m-0">EzSolax Copyright @2023</p>
+        </div>
+    )
+}
