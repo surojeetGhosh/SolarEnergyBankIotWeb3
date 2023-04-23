94d09d7f83630f1ccd734b832f92f9a00da3c848
Surojeet <surojeetghosh89@gmail.com>
2023-04-23 15:22:55 +0530

    Changed useEffect

--- hardhat-solar-trading/scripts/checkMachineStatus.js
+++ hardhat-solar-trading/scripts/checkMachineStatus.js
@@ -6,11 +6,11 @@ async function main() {
 
     const Store = await ethers.getContract("Solar", deployer);
     
-    //  (await Store.setMachine("1")).wait(1);
+    //  (await Store.setMachine("0")).wait(1);
     // (await Store.stopMachine("1")).wait(1);
-    // const status = await Store.getMachineState("1");
-    // const currentUser = await Store.currentUser("1");
-    // console.log(status.toString(), " ", currentUser.toString());
+    const status = await Store.getMachineState("1");
+    const currentUser = await Store.currentUser("1");
+    console.log(status.toString(), " ", currentUser.toString());
 }
 
 
